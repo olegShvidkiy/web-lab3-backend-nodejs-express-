ecdb498952a3e8efd74838e29b610739cfd69e6b
bladeee01 <54937475+olegShvidkiy@users.noreply.github.com>
2021-05-25 03:19:47 +0300

    Delete itemModel.js

--- itemModel.js
+++
@@ -1,9 +0,0 @@
-module.exports = class ItemModel {
-  constructor(title) {
-    this.title = title;
-    this.id = Math.round(Math.random() * 100000).toString();
-    this.chooseNumberVotes = 0;
-    this.percent = 0;
-    this.chosed = false;
-  }
-};
